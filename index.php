75c15ad07ef1e8d2f040b6d0a2528bcdf9134140
che <che@example.com>
2023-12-28 18:03:12 +0300

    adding inde.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function($class) {
+    $formattedClass = str_replace("\\", "/", $class);
+    $path = "{$formattedClass}.php";
+    require_once $path;
+});
+
+use App\Helper;
+
+$helper = new Helper('Mariya');
+print_r($helper->getName());
